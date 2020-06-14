2fd244697fad08cfb971717dae01b23379358df2
NovaFox161 <novamaday@gmail.com>
2020-06-14 18:06:55 -0500

    Add oauth-apps endpoint response mappings

---
+++ src/main/java/rocket4j/json/response/oauthapps/GetResponse.java
@@ -0,0 +1,34 @@
+/*
+ * This file is part of rocket-chat-json.
+ *
+ * rocket-chat-json is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rocket-chat-json is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with rocket-chat-json.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package rocket4j.json.response.oauthapps;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.immutables.value.Value;
+import rocket4j.json.oauth.App;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableGetResponse.class)
+@JsonDeserialize(as = ImmutableGetResponse.class)
+public interface GetResponse {
+
+    App oauthApp();
+
+    @JsonProperty("success")
+    boolean successful();
+}
---
+++ src/test/java/rocket4j/json/response/oauthapps/GetResponseTest.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of rocket-chat-json.
+ *
+ * rocket-chat-json is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rocket-chat-json is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with rocket-chat-json.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package rocket4j.json.response.oauthapps;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import rocket4j.json.JsonFile;
+import rocket4j.json.JsonTests;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@JsonTests
+public final class GetResponseTest {
+
+    private final ObjectMapper objectMapper;
+
+    public GetResponseTest(final ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Test
+    void deserializeInfoResponseTest(@JsonFile("get-example-result.json") final String json) {
+        assertDoesNotThrow(() -> objectMapper.readValue(json, GetResponse.class));
+    }
+}
+
