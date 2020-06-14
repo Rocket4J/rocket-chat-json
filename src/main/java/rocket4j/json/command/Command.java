a393656ad699884586a708df7216551b3946207a
NovaFox161 <novamaday@gmail.com>
2020-06-14 00:20:32 -0500

    Add commands endpoints response mappings

---
+++ src/main/java/rocket4j/json/command/Command.java
@@ -0,0 +1,36 @@
+/*
+ * This file is part of rocket-chat-json.
+ *
+ * rocket-chat-json is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rocket-chat-json is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with rocket-chat-json.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package rocket4j.json.command;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import java.util.Optional;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableCommand.class)
+@JsonDeserialize(as = ImmutableCommand.class)
+public interface Command {
+
+    String command();
+
+    Optional<String> params();
+
+    Optional<String> description();
+
+    boolean clientOnly();
+}
