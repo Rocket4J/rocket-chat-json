07f163ba913596f55c50eb71b3e837b13176f5d7
NovaFox161 <novamaday@gmail.com>
2020-06-14 17:02:32 -0500

    Add permissions endpoint response mappings
    Would add permissions.listAll but the doc page is 404ing

---
+++ src/main/java/rocket4j/json/response/permissions/UpdateResponse.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of rocket-chat-json.
+ *
+ * rocket-chat-json is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rocket-chat-json is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with rocket-chat-json.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package rocket4j.json.response.permissions;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import java.util.List;
+import org.immutables.value.Value;
+import rocket4j.json.permission.Permission;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableUpdateResponse.class)
+@JsonDeserialize(as = ImmutableUpdateResponse.class)
+public interface UpdateResponse {
+
+    List<Permission> permissions();
+
+    @JsonProperty("success")
+    boolean successful();
+}
